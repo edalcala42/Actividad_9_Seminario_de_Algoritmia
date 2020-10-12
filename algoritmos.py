30878b31c843f80e038e2b2899d202d5526658ff
Eduardo Cabrales Alcalá <eduardo.cabrales7500@alumnos.udg.mx>
2020-10-12 17:05:09 -0500

    Se sustitiyó el signo de resta por el de suma en la fórmula. También se cambió el nombre de la función a distancia euclidiana.

--- algoritmos.py
+++ algoritmos.py
@@ -1,9 +1,7 @@
 import math
 
-def distancia_eucladiana(x_1:int, y_1:int, x_2:int, y_2:int)->float:
-    #raiz: math.sqr()
+def distancia_euclidiana(x_1:int, y_1:int, x_2:int, y_2:int)->float:
     x = x_2 - x_1
     y = y_2 - y_1
-
-    return (math.sqrt((x*x)-(y*y)))
-
+    return (math.sqrt((x*x)+(y*y)))
+    
